7a2908e40b0518df52598a451fa09e5520f39a8e
Akshay Kumar Sharma <akshayne912@gmail.com>
2023-11-11 10:58:24 +0000

    second commit

---
+++ src/Components/projectList.jsx
@@ -0,0 +1,37 @@
+/**
+ * Project list
+ *
+ * An array of objects that will be used to display for your project
+ * links section. Below is a sample, update to reflect links you'd like to highlight.
+ */
+export const projectList = [
+  {
+    title: "Dynamic Web Solutions with AWS Integration 🚀",
+    description:
+      "Crafted a series of websites integrating AWS services for optimal performance and functionality.",
+    url: "https://github.com/Cappybara12/",
+  },
+  {
+    title: "Checkout My(AKA DEV_VOYAGER) Portfolio Website 🚀",
+    description: "Created my very own portfolio website using my tech stack .",
+    url: "https://dev-voyager.netlify.app/",
+  },
+  {
+    title: "Hackathon Expertise 🏆",
+    description:
+      "Participated ,mentored,judges and  excelled in multiple hackathons, showcasing my coding prowess and ability to meet tight deadlines.",
+    url: "https://www.linkedin.com/in/akshay-kumar-sharma-37aa55256/recent-activity/all/",
+  },
+  {
+    title: "AWS Certified Developer",
+    description:
+      "Achieved AWS certifications with 10+ badges, demonstrating expertise in cloud technologies.",
+    url: "https://www.linkedin.com/in/akshay-kumar-sharma-devvoyager/details/certifications/",
+  },
+  {
+    title: "Web Dev for Real-world Applications",
+    description:
+      "Created impactful web solutions addressing real-world challenges, merging creativity with technical excellence.",
+    url: "https://github.com/Cappybara12/WIEHACK_BVP",
+  },
+];
